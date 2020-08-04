4c03362362f0c49f5d9147d77a5825f2fc94c755
sai-myne <ahlusir@gmail.com>
2020-08-04 15:53:07 +0800

    Adding New Request

--- functions/index.js
+++ functions/index.js
@@ -23,3 +23,25 @@ exports.userDeleted = functions
       const doc = admin.firestore().collection('users').doc(user.uid);
       return doc.delete();
   });
+
+// http callable function (adding a request)
+exports.addRequest = functions
+  .region('asia-southeast2')
+  .https.onCall((data, context) => {
+    if(!context.auth){
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'only authenticated users can add requests'
+        );
+    }
+    if(data.text.length > 30){
+        throw new functions.https.HttpsError(
+            'invalid-argument',
+            'request must be no more than 30 characters long'
+        );
+    }
+    return admin.firestore().collection('requests').add({
+        text: data.text,
+        upvotes: 0,
+    });
+  });
