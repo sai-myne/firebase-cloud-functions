ce90c64701f3ab01cf8233a30f44b37be2dc8b25
sai-myne <ahlusir@gmail.com>
2020-08-04 16:48:22 +0800

    Adding Request Front End

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
-
+const requestForm = document.querySelector('.new-request form')
 // open request modal
 requestLink.addEventListener('click', () => {
     requestModal.classList.add('open');
@@ -11,5 +11,23 @@ requestModal.addEventListener('click', (e) => {
     if(e.target.classList.contains('new-request')){
         requestModal.classList.remove('open');
     }
+});
+
+// add a new request
+requestForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const functions = firebase.app().functions('asia-southeast2');
+    const addRequest = functions.httpsCallable('addRequest');
+    addRequest({
+        text: requestForm.request.value,
+    })
+    .then(() => {
+        requestForm.reset();
+        requestModal.classList.remove('open');
+        requestForm.querySelector('.error').textContent = '';
+    })
+    .catch((err) => {
+        requestForm.querySelector('.error').textContent = err.message;
+    });
 });
 
