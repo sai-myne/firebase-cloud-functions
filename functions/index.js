a24abc957d86d5154329ca2fada039d2afb26507
sai-myne <ahlusir@gmail.com>
2020-08-04 15:42:19 +0800

    Adding New Requests

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // auth trigger (new user signup)
 exports.newUserSignup = functions
   .region("asia-southeast2")
   .auth.user()
   .onCreate((user) => {
-      console.log('user created', user.email, user.uid);
+      // for background triggers you must return a value/promise
+      return admin.firestore().collection('users').doc(user.uid).set({
+          email: user.email,
+          upvoteOn: []
+      });
   });
 
 // auth trigger( user deleted)
@@ -13,5 +19,7 @@ exports.userDeleted = functions
   .region("asia-southeast2")
   .auth.user()
   .onDelete((user) => {
-      console.log('user deleted', user.email, user.uid);
+      // for background triggers you must return a value/promise
+      const doc = admin.firestore().collection('users').doc(user.uid);
+      return doc.delete();
   });
