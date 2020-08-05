fa028f13f80678e6276ba11c0c5fc548c3be12b2
sai-myne <ahlusir@gmail.com>
2020-08-05 09:44:50 +0800

    upvoting function

--- functions/index.js
+++ functions/index.js
@@ -45,3 +45,38 @@ exports.addRequest = functions
         upvotes: 0,
     });
   });
+
+// upvote callable function
+exports.upvote = functions.region('asia-southeast2').https.onCall((data, context) => {
+    // check auth state
+    if(!context.auth){
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'only authenticated users can add requests'
+        );
+    }
+    // get refs for user doc & request doc
+    const user = admin.firestore().collection('users').doc(context.auth.uid);
+    const request = admin.firestore().collection('requests').doc(data.id);
+
+    return user.get().then(doc => {
+        // check user hasn't already upvoted the request
+        if(doc.data().upvoteOn.includes(data.id)){
+            throw new functions.https.HttpsError(
+                'failed-precondition',
+                'You can only upvote something once'
+            );
+        }
+
+        // update user array
+        return user.update({
+            upvoteOn: [...doc.data().upvoteOn, data.id]
+        })
+        .then(() => {
+            // update votes on the request
+            return request.update({
+                upvotes: admin.firestore().FieldValue.increment(1)
+            })
+        })
+    })
+});
